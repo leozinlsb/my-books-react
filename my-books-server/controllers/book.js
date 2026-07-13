4e6973306ed8e4a05c7bff1dc2556f5e5cb6ee98
leozinlsb <leonardosilvab007@gmail.com>
2026-07-12 21:00:19 -0300

    feat: adicionado validação de IDs e body ao enviar uma requisição, não será possível executar um metódo HTTP enviando um ID inválido, inexistente ou um body inválido

--- my-books-server/controllers/book.js
+++ my-books-server/controllers/book.js
@@ -1,61 +1,87 @@
 const fs = require("fs")
 const { getAllBooks, getBookById, insertBook, modifyBook, deleteBookById } = require("../services/books")
 
-function getBooks (req, res){
-    try{
-        const books =  getAllBooks()
+function getBooks(req, res) {
+    try {
+        const books = getAllBooks()
         res.send(books)
-    } catch(error) {
+    } catch (error) {
         res.status(500)
         res.send(error.message)
     }
 }
 
 function getBook(req, res) {
-    try{
+    try {
         const id = req.params.id
-        const book = getBookById(id)
 
-        res.send(book)
+        if (id && Number(id)) {
+            const book = getBookById(id)
+            res.send(book)
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
+
     } catch (error) {
-        res.status(500)
+        res.status(404)
         res.send(error.message)
     }
 }
 
-function postBook (req, res){
-    try{
+function postBook(req, res) {
+    try {
         const newBook = req.body
-        insertBook(newBook)
 
-        res.status(201)
-        res.send("Livro inserido com sucesso")
+        if (req.body.nome) {
+            insertBook(newBook)
+            res.status(201)
+            res.send("Livro inserido com sucesso")
+        } else {
+            res.status(422)
+            res.send("Nome é obrigatório")
+        }
+
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
     }
 }
 
-function patchBook (req, res){
-    try{
+function patchBook(req, res) {
+    try {
         const id = req.params.id
         const body = req.body
 
-        modifyBook(body, id)
-        res.send("Item modificado")
-    } catch (error){
-        res.status(500)
+        if (id && Number(id) && req.body.nome) {
+            modifyBook(body, id)
+            res.send("Item modificado")
+        } else {
+            res.status(422)
+            res.send("Body ou Id inválidos (nome é obrigatório)")
+        }
+
+
+    } catch (error) {
+        res.status(404)
         res.send(error.message)
     }
 }
 
-function deleteBook (req, res){
-    try{
+function deleteBook(req, res) {
+    try {
         const id = req.params.id
 
-        deleteBookById(id)
-        res.send("Deletado com sucesso")
-    } catch (error){
+        if (id && Number(id)) {
+            deleteBookById(id)
+            res.send("Deletado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
+
+    } catch (error) {
         res.status(500)
         res.send(error.message)
     }
--- my-books-server/services/books.js
+++ my-books-server/services/books.js
@@ -1,39 +1,47 @@
 const fs = require("fs")
 
-function getAllBooks(){
-    return JSON.parse( fs.readFileSync("books.json") )
+function getAllBooks() {
+    return JSON.parse(fs.readFileSync("books.json"))
 }
 
-function getBookById(id){
-    const books = JSON.parse( fs.readFileSync("books.json"))
+function getBookById(id) {
+    const books = JSON.parse(fs.readFileSync("books.json"))
 
-    const filteredBook = books.filter( book => book.id === id)[0]
+    const filteredBook = books.filter(book => book.id === id)[0]
+
+    if (filteredBook === undefined) {
+        throw new Error("Livro não encontrado")
+    }
 
     return filteredBook
 }
 
-function insertBook(newBook){
-    const books = JSON.parse( fs.readFileSync("books.json") )
+function insertBook(newBook) {
+    const books = JSON.parse(fs.readFileSync("books.json"))
 
     const newBookList = [...books, newBook]
 
     fs.writeFileSync("books.json", JSON.stringify(newBookList))
 }
 
-function modifyBook(patchedBook, id){
-    let books = JSON.parse( fs.readFileSync("books.json") )
-    const modifyIndex = books.findIndex(book => book.id === id)
+function modifyBook(patchedBook, id) {
+    let books = JSON.parse(fs.readFileSync("books.json"))
+    const modifyIndex = books.findIndex(book => book.id === id) //when the method .findIndex dont find anything it returns -1
 
-    const modifiedContent = {...books[modifyIndex], ...patchedBook}
+    if (modifyIndex === -1) {
+        throw new Error("Livro não encontrado")
+    }
+
+    const modifiedContent = { ...books[modifyIndex], ...patchedBook }
 
     books[modifyIndex] = modifiedContent
 
     fs.writeFileSync("books.json", JSON.stringify(books))
 }
 
-function deleteBookById(id){
-    let books = JSON.parse( fs.readFileSync("books.json") )
-    const filteredBooks = books.filter( book => book.id !== id)
+function deleteBookById(id) {
+    let books = JSON.parse(fs.readFileSync("books.json"))
+    const filteredBooks = books.filter(book => book.id !== id)
 
     fs.writeFileSync("books.json", JSON.stringify(filteredBooks))
 }
